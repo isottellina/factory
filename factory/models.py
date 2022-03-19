af1d6143ab7cc5b0b2a93d0bdc66b4e495f9f784
Louise <louise@zanier.org>
2022-03-19 15:33:58 +0100

    Added the rest of the models needed

--- factory/models.py
+++ factory/models.py
@@ -1,9 +1,21 @@
 import enum
+import uuid
 
 import sqlalchemy as sa
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import Mapped, declarative_base, declared_attr, relationship
 
 Base = declarative_base()
+
+
+class PKMixin:
+    """
+    This adds a primary key to models. Even though most of the models
+    have a column that is unique to a single object, we still have an
+    auto-incrementing PK because most of the others are strings, and
+    possibly subject to change in subsequent versions.
+    """
+
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
 
 
 class RobotAction(enum.Enum):
@@ -14,10 +26,9 @@ class RobotAction(enum.Enum):
     BUYING_ROBOT = enum.auto()
 
 
-class Robot(Base):
+class Robot(Base, PKMixin):
     __tablename__ = "robot"
 
-    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     name = sa.Column(
         sa.String(64),
         unique=True,
@@ -40,3 +51,65 @@ class Robot(Base):
         nullable=True,
         doc="When this robot will be done with its current task.",
     )
+
+
+def gen_uuid() -> str:
+    """
+    We have to generate a string UUID, since SQLite doesn't support the UUID type.
+    """
+    return uuid.uuid4().hex
+
+
+class MiningProductMixin:
+    """
+    This is a mixin for Foo and Bar, seeing as they share most of their columns.
+    We don't make them into a single table, however, to ensure consistency.
+    """
+
+    serial = sa.Column(
+        sa.String(32), default=gen_uuid, doc="For traceability purposes."
+    )
+    used = sa.Column(
+        sa.Boolean,
+        default=False,
+        nullable=False,
+        doc="Has this product been used to make a Foobar?",
+    )
+
+    @declared_attr
+    def miner_id(cls) -> sa.Column[sa.Integer]:
+        return sa.Column(sa.Integer, sa.ForeignKey(Robot.id))
+
+    @declared_attr
+    def mined_by(cls) -> Mapped[Robot]:
+        return relationship(Robot)
+
+
+class Foo(Base, PKMixin, MiningProductMixin):
+    __tablename__ = "foo"
+
+
+class Bar(Base, PKMixin, MiningProductMixin):
+    __tablename__ = "bar"
+
+
+class Foobar(Base, PKMixin):
+    __tablename__ = "foobar"
+
+    sold = sa.Column(
+        sa.Boolean, default=False, nullable=False, doc="Has this Foobar been sold?"
+    )
+
+    foo_used_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey(Foo.id),
+        doc="The Foo used to make this Foobar",
+    )
+    bar_used_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey(Bar.id),
+        doc="The Bar used to make this Foobar",
+    )
+
+    foo_used: Foo = relationship(Foo)
+    bar_used: Bar = relationship(Bar)
